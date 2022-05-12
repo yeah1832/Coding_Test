28cb5ab48c39eddd11d455a0b2c19f895424b6b0
yeah1832 <aer561@naver.com>
2022-05-13 00:52:34 +0900

    Create 콜라츠 추측.cpp

---
+++ 콜라츠 추측.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(int num) {
+    int answer = 0;
+
+    while(num != 1){
+        answer++;
+        if(answer > 500){
+            answer = -1;
+            break; 
+        }
+        if(num % 2 == 0){
+            num /=2;
+        }else if(num % 2 == 1){
+            num = num * 3 + 1;
+        }
+    }
+   
+    
+    return answer;
+}
