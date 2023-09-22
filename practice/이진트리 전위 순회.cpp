8a737b9ad613ebcffd2c406fa6b4f277ae305dac
yeah1832 <aer561@naver.com>
2023-09-22 14:24:29 +0900

    Create 이진트리 전위 순회.cpp

---
+++ practice/이진트리 전위 순회.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+using namespace std;
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+void D(int v){
+	if(v>7) return;
+	else{
+		printf("%d", v);
+		D(2*v);
+		D(2*v+1);
+	}
+}
+
+int main(int argc, char** argv) {
+	freopen("input.txt", "rt", stdin);
+	D(1);	
+	return 0;
+}
